09438588e6732bce778543e98ab2d974ec55f9b8
ankitsinghrana13 <ak9897432538@gmail.com>
2022-02-28 06:17:56 +0800

    Add project files.

---
+++ StudentApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+ namespace StudentApp
+{
+
+    class hello
+    {
+        public string name;
+        public int age;
+        public int salary;
+
+        public void Getdata(string name, int age, int salary)
+        {
+            this.name = name;
+            this.age = age;
+            this.salary = salary;
+            Console.WriteLine("Name=" + this.name);
+            Console.WriteLine("age=" + this.age);
+            Console.WriteLine("salary=" + this.salary);
+        }
+    }
+    class program
+    {
+static void Main(string[] arg)
+        {
+            hello obj1 = new hello();
+           
+            obj1.Getdata("ankit",23,25000);
+
+        }
+    }
+
+}
